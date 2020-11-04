2e2f13bc5363a3f3c5f029dceb5a4e7ea38883a9
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-11-03 23:12:28 -0300

    Invertendo camisa

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+const item = document.querySelector('.item');
 
 const title = document.querySelector('.title');
 const sneaker = document.querySelector('.sneaker img');
@@ -16,20 +17,27 @@ container.addEventListener('mousemove', e => {
 //Animate In
 container.addEventListener("mouseenter", e => {
   card.style.transition = 'none';
-  
-  title.style.transform = "translateZ(150px)";
-  sneaker.style.transform = "translateZ(200px) rotateZ(-360deg)";
-  description.style.transform = "translateZ(125px)";
-  sizes.style.transform = "translateZ(100px)";
-  purchase.style.transform = "translateZ(75px)";
+ 
+  //item.style.transform = "translateZ(200px) rotateZ(0deg)";
+  item.classList.toggle('is-flipped');
+
+  title.style.transform = "translateZ(40px)";
+  //sneaker.style.transform = "translateZ(200px) rotateZ(-0deg)";
+  description.style.transform = "translateZ(30px)";
+  sizes.style.transform = "translateZ(25px)";
+  purchase.style.transform = "translateZ(20px)";
 })
 
 //Animate Out
 container.addEventListener("mouseleave", e => {
+
+  //item.style.transform = "translateZ(0px) rotateZ(0deg)";
+  item.classList.toggle('is-flipped');
+
   card.style.transition = 'all 0.5s ease';
   card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   title.style.transform = "translateZ(0px)";
-  sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
+  //sneaker.style.transform = "translateZ(0px) rotateZ(0deg)";
   description.style.transform = "translateZ(0px)";
   sizes.style.transform = "translateZ(0px)";
   purchase.style.transform = "translateZ(0px)";
